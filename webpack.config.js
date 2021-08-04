e71c7115955ea9a2fc693360d8e636ed5b4175dd
Stephen Scaff <smscaff@gmail.com>
2021-08-03 17:02:02 -0700

    getting less and quries to work

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
-const webpack = require('webpack');
-const { configBuilder } = require('@marko/build');
+const path = require("path");
+const webpack = require("webpack");
+const { configBuilder } = require("@marko/build");
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === "production";
 const { getServerConfig, getBrowserConfigs } = configBuilder({
-  entry: path.join(__dirname, 'src/pages'),
-  production
+  entry: path.join(__dirname, "src/pages"),
+  production,
 });
 
 module.exports = [
-  ...getBrowserConfigs(config => {
+  ...getBrowserConfigs((config) => {
     shared(config);
     return config;
   }),
-  getServerConfig(config => {
+  getServerConfig((config) => {
     shared(config);
     return config;
-  })
+  }),
 ];
 
 function shared(config) {
   config.resolve = {
     ...config.resolve,
     alias: {
-      Assets: path.resolve(__dirname, 'src/assets'),
-      Client: path.resolve(__dirname, 'src/client'),
-      Constants: path.resolve(__dirname, 'src/constants'),
-      Data: path.resolve(__dirname, 'src/data'),
-      Utils: path.resolve(__dirname, 'src/utils'),
-      Styles: path.resolve(__dirname, 'src/styles')
-    }
+      Assets: path.resolve(__dirname, "src/assets"),
+      Api: path.resolve(__dirname, "src/api"),
+      Constants: path.resolve(__dirname, "src/constants"),
+      Data: path.resolve(__dirname, "src/data"),
+      Utils: path.resolve(__dirname, "src/utils"),
+      Styles: path.resolve(__dirname, "src/styles"),
+    },
   };
 }
